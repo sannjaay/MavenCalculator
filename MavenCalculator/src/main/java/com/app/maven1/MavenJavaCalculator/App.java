dc4d29596ac300add9f55ab5b1de8ac4f50a8efd
sanjay <157896937+sannjaay@users.noreply.github.com>
2025-01-18 10:34:35 +0530

    Add files via upload

---
+++ MavenCalculator/src/main/java/com/app/maven1/MavenJavaCalculator/App.java
@@ -0,0 +1,12 @@
+package com.app.maven1.MavenJavaCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	
+    public static void main( String[] args )
+    {
+        System.out.println("hello");    }
+}
